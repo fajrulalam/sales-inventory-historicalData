1e83cc87987bd7e9871b53f3b8836950062c7e38
fajrulalam <71478225+fajrulalam@users.noreply.github.com>
2022-02-23 13:59:19 +0700

    added a drop menu

--- app/src/main/java/com/example/dashboardandinventory2/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/dashboardandinventory2/ui/gallery/GalleryFragment.java
@@ -1,9 +1,12 @@
 package com.example.dashboardandinventory2.ui.gallery;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +22,7 @@ public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
+    private AutoCompleteTextView autoCompleteTextView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,13 +32,23 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        autoCompleteTextView = root.findViewById(R.id.autoCompleteTextView2);
+
+        String[] granularity = getResources().getStringArray(R.array.granularity);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), R.layout.dropdown_granularity, granularity);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        autoCompleteTextView.setInputType(View.AUTOFILL_TYPE_NONE);
+
+
+
+
+//        final TextView textView = binding.textGallery;
+//        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
